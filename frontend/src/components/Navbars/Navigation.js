d15c406a69a87abbb2d9b210f2d9f4280a4f2ce6
k.sarath kumar <166106731+sarathkumargit@users.noreply.github.com>
2025-01-25 19:48:53 +0530

    new

--- frontend/src/components/Navbars/Navigation.js
+++ frontend/src/components/Navbars/Navigation.js
@@ -5,27 +5,35 @@ import Logo from '../../images/tripbuddy-logo.svg';
 function Navigation() {
     return (
         <nav style={styles.nav}>
-            <img src={Logo} alt="TripBuddy Logo" style={styles.logo} />
-            <div style={styles.links}>
-                <Link to="/" style={styles.link}>Home</Link>
-                <Link to="/destinations" style={styles.link}>Destinations</Link>
-                <Link to="/admin" style={styles.link}>Admin</Link>
-                <Link to="/managedestinations" style={styles.link}>Manage Destinations</Link>
-            </div>
-        </nav>
+        <img src={Logo} alt="TripBuddy Logo" style={styles.logo} />
+        <div style={styles.links}>
+            <Link to="/" style={styles.link}>Home</Link>
+            <Link to="/destinations" style={styles.link}>Destinations</Link>
+            <Link to="/managedestinations" style={styles.link}>Plans</Link>
+        </div>
+    </nav>
+    
     );
 }
 
 const styles = {
     nav: {
         display: 'flex',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'red', // Navy Blue
         padding: '10px 20px',
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        zIndex: 1000,
+        backgroundColor: 'rgba(247, 245, 245, 0.3)', // Semi-transparent background
+        backdropFilter: 'blur(10px)', // Glass-like effect
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Subtle shadow for depth
     },
     logo: {
         height: '40px',
+        position: 'absolute',
+        left: '20px', // Distance from the left side
     },
     links: {
         display: 'flex',
@@ -33,10 +41,13 @@ const styles = {
     },
     link: {
         textDecoration: 'none',
-        color: 'white',
+        color: 'black',
         fontSize: '18px',
         fontWeight: 'bold',
+        transition: 'color 0.3s',
+        textAlign: 'center'
     },
 };
+
 
 export default Navigation;
