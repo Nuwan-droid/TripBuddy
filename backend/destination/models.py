504a6faba39578834125c1052e8f7e8090407039
haamidh <haamidhansak@gmail.com>
2025-01-25 11:18:12 +0530

    change the destination model
    changed the destination model according to the db table

--- backend/destination/models.py
+++ backend/destination/models.py
@@ -4,10 +4,15 @@ from django.db import models
 class Destination(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # location = models.TextField()
+    location = models.TextField()
     image_url = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        db_table = 'destinations'
+        verbose_name = 'Destination'
+        verbose_name_plural = 'Destinations'
