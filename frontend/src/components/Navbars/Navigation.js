f1dc88f75eae5440994fa9f2ced72d23703063ba
k.sarath kumar <166106731+sarathkumargit@users.noreply.github.com>
2025-01-27 14:22:13 +0530

    latest frontend

--- frontend/src/components/Navbars/Navigation.js
+++ frontend/src/components/Navbars/Navigation.js
@@ -1,53 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../images/tripbuddy-logo.svg';
+import { Link } from 'react-router-dom'; 
+import logo from '..//.//..//assets/img/logo.jpeg';
 
 function Navigation() {
     return (
         <nav style={styles.nav}>
-        <img src={Logo} alt="TripBuddy Logo" style={styles.logo} />
-        <div style={styles.links}>
-            <Link to="/" style={styles.link}>Home</Link>
-            <Link to="/destinations" style={styles.link}>Destinations</Link>
-            <Link to="/managedestinations" style={styles.link}>Plans</Link>
-        </div>
-    </nav>
-    
+            
+            <img src= {logo} alt="TripBuddy Logo" style={styles.logo} />
+
+           
+            <div style={styles.navLinksContainer}>
+                <Link to="/" style={styles.navLink}>Home</Link>
+                <Link to="/destinations" style={styles.navLink}>Destinations</Link>
+                <Link to="/plans" style={styles.navLink}>Plans</Link>
+                <Link to="/admin" style={styles.navLink}>Admin</Link>
+                {/* <Link to="/managedestinations" style={styles.navLink}>Manage Destinations</Link> */}
+            </div>
+        </nav>
     );
 }
 
 const styles = {
     nav: {
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'space-between',
         padding: '10px 20px',
-        position: 'fixed',
-        top: 0,
-        width: '100%',
-        zIndex: 1000,
-        backgroundColor: 'rgba(247, 245, 245, 0.3)', // Semi-transparent background
-        backdropFilter: 'blur(10px)', // Glass-like effect
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Subtle shadow for depth
+        backgroundColor: 'rgba(6, 120, 105, 0.4)', 
     },
     logo: {
-        height: '40px',
-        position: 'absolute',
-        left: '20px', // Distance from the left side
+        height: '50px',
     },
-    links: {
+    navLinksContainer: {
         display: 'flex',
-        gap: '20px',
+        gap: '40px',
     },
-    link: {
+    navLink: {
         textDecoration: 'none',
-        color: 'black',
+        color: 'black', // White text for better contrast on the navbar
         fontSize: '18px',
         fontWeight: 'bold',
-        transition: 'color 0.3s',
-        textAlign: 'center'
     },
 };
-
 
 export default Navigation;
