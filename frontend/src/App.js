219364f0408b6192d937c53af41da85786ac627a
k.sarath kumar <166106731+sarathkumargit@users.noreply.github.com>
2025-01-28 13:25:50 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navbars/Navigation'; // Adjust path if needed
-import Destinations from './pages/Destinations';
-import Home from './pages/Home';
-import Admin from './layouts/Admin';
-import plans from './pages/plans';
-import ManageDestinations from './pages/AdminDestination';
-
+import Navigation from './components/Navbars/Navigation.js'; // Adjust path if needed
+import Destinations from './pages/Destinations.js';
+import Home from './pages/Home.js';
+import Admin from 'layouts/Admin.js';
+import ManageDestinations from './pages/AdminDestination.js';
+import Login from './components/LoginRegister/LoginRegister.jsx';
 function App() {
     return (
         <Router>
             <div className="App">
-                {/* Navigation Component (Navbar) */}
                 <Navigation />
-
-                {/* Routes */}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/destinations" element={<Destinations />} />
                     <Route path="/admin" element={<Admin />} />
                     <Route path="/managedestinations" element={<ManageDestinations />} />
-                    <Route path="/plans" element={<plans />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
         </Router>
