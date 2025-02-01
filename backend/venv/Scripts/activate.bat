9ca42d72f5aadac38bc4ae8ec86d0c7e8324d004
charith99-star <cst21067@std.uwu.ac.lk>
2025-02-01 12:01:54 +0530

    add venv file

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Charith Jayasinghe\OneDrive\Desktop\projects\3.1\AdV_Django_project\projects\backend\venv
+set VIRTUAL_ENV=D:\apt\TripBuddy\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
